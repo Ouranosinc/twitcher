e56275e58a8d237de3b93b9680787395455c0f6b
Félix Gagnon-Grenier <felix.gagnon-grenier@crim.ca>
2018-09-25 11:54:17 -0400

    configuring app

--- twitcher/wps_restapi/__init__.py
+++ twitcher/wps_restapi/__init__.py
@@ -1,12 +1,9 @@
 from pyramid.settings import asbool
 from twitcher.wps_restapi.frontpage import frontpage
 from twitcher.wps_restapi import swagger_definitions as sd
-from pyramid.httpexceptions import *
+from twitcher.adapter import jobstore_factory
 from twitcher.wps_restapi.api import api_swagger_json, api_swagger_ui, api_versions
-from twitcher.db import MongoDB
 import logging
-from twitcher.owsexceptions import *
-from twitcher.exceptions import ServiceNotFound
 logger = logging.getLogger(__name__)
 
 
@@ -33,4 +30,4 @@ def includeme(config):
                         request_method='GET', renderer='templates/swagger_ui.mako')
         config.add_view(api_versions, route_name=sd.api_versions_service.name,
                         request_method='GET', renderer='json')
-        config.registry.celerydb = MongoDB.get(config.registry)
+        config.registry.celerydb = jobstore_factory(config.registry)
